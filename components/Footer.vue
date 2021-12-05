e311b23ac2d17c131c04525f43129f0df55f394c
Marcelo Munhoz <me@marcelomunhoz.com>
2021-12-05 01:53:48 -0300

    Footer added

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="mt-5 text-center">
+    <p class="small">
+      This site is built with
+      <a href="https://nuxtjs.org" class="font-weight-bold text-primary">Nuxt</a>
+      and hosted on <a href="https://www.netlify.com" class="font-weight-bold text-primary">Netlify</a>. The source code is hosted on
+      <a href="https://github.com" class="font-weight-bold text-primary">Github</a>.
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
