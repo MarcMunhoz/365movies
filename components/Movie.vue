afd63de34f452fbf67c696f6adb8f33d1415099d
Marcelo Munhoz <me@marcelomunhoz.com>
2021-12-15 17:08:18 -0300

    API data

--- components/Movie.vue
+++ components/Movie.vue
@@ -3,64 +3,62 @@
     <v-row>
       <v-btn @click="getMovies" elevation="2" depressed color="primary" class="mx-auto">Movie of the day</v-btn>
     </v-row>
-    <v-row>
-      <template v-for="n in 4">
-        <v-col :key="n" class="mt-2" cols="12">
-          <strong>Category {{ n }}</strong>
-        </v-col>
-
-        <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
-          <v-sheet height="150"></v-sheet>
-        </v-col>
-      </template>
-    </v-row>
+    <!-- <v-row>
+      <ul>
+        <li v-for="movie in movies" :key="movie">
+          {{ movie.Title }}
+        </li>
+      </ul>
+    </v-row> -->
   </v-container>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
 const api_key = process.env.OMDBAPI_KEY
 
-export default Vue.extend({
+export default {
   name: 'Movie',
   data() {
     return {
-      movie: Object,
+      movies: [],
+      movieCnt: 0,
     }
   },
   methods: {
-    async getMovies() {
-      const rndNumber = Math.floor(Math.random() * (9999999 - 1 + 1)) + 1
-      const movieNumber = `tt${String(rndNumber).padStart(7, '0')}`
+    getMovies() {
+      const url = `https://www.omdbapi.com/`
+      let counter = 1
 
-      const url = `http://www.omdbapi.com/`
+      while (this.movieCnt < 11) {
+        let rndNumber = Math.floor(Math.random() * (9999999 - 1 + 1)) + 1
+        let movieNumber = `tt${String(rndNumber).padStart(7, '0')}`
 
-      try {
-        const resp = await this.$axios.$get(url, {
-          params: {
-            apikey: api_key,
-            type: 'movie',
-            i: movieNumber,
-          },
-        })
-
-        this.movie = resp
-        return console.log(this.movie)
-      } catch (err) {
-        return console.error(err)
+        try {
+          this.$axios
+            .$get(url, {
+              params: {
+                apikey: api_key,
+                i: movieNumber,
+              },
+            })
+            .then((resp) => {
+              if (resp.Response === 'True') {
+                return resp.Type === 'movie' && this.movies.push(resp), this.movieCnt++
+              } else {
+                throw new Error(resp.Error)
+              }
+            })
+            .catch((err) => {
+              return console.log(err)
+            })
+        } catch (error) {
+          return console.log(error)
+        }
       }
 
-      // this.$axios.interceptors.response.use(
-      //   (response) => {
-      //     console.log(response)
-
-      //     return response
-      //   },
-      //   (error) => {
-      //     return Promise.reject(error)
-      //   }
-      // )
+      console.log(this.movies)
+      console.log(this.movieCnt)
     },
   },
-})
+}
 </script>
