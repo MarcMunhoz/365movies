64cca21101a65c71cb2647ab9df6eca89bc3893b
Marcelo Munhoz <me@marcelomunhoz.com>
2022-01-14 18:12:03 -0300

    About page was created

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-row>
+        <h1>About it</h1>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  layout: 'default',
+  name: 'about',
+})
+</script>
+
+<style></style>
