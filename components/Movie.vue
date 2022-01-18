496ebf59db5aeb7442f4da46f291d97110edb8c9
Marcelo Munhoz <me@marcelomunhoz.com>
2022-01-18 10:35:20 -0300

    F*k up rapidapi headers

--- components/Movie.vue
+++ components/Movie.vue
@@ -54,7 +54,6 @@
 
 <script>
 const api_key = process.env.RAPIDAPI_KEY
-const api_host = process.env.RAPIDAPI_HOST
 
 export default {
   name: 'Movie',
@@ -98,7 +97,7 @@ export default {
       async function ftMovie() {
         let resp = await fetch(flashURL, {
           method: 'get',
-          headers: { 'x-rapidapi-host': api_host, 'x-rapidapi-key': api_key },
+          headers: { 'x-rapidapi-host': 'imdb-internet-movie-database-unofficial.p.rapidapi.com', 'x-rapidapi-key': api_key },
           useQueryString: true,
         })
 
