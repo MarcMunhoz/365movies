7375decd554afa658badcd9af4bc4102a55cef25
Marcelo Munhoz <me@marcelomunhoz.com>
2025-04-24 13:16:02 -0300

    Agenda: Options API converted to Composition API

--- app/src/pages/agenda.vue
+++ app/src/pages/agenda.vue
@@ -103,211 +103,181 @@
   </q-page>
 </template>
 
-<script>
+<script setup>
 import { ref, computed, watch } from "vue";
 import { Calendar } from "v-calendar";
 import { useScreens } from "vue-screen-utils";
 import "v-calendar/style.css";
-import { useQuasar } from "quasar";
-import { getLocalStorage, setLocalStorage } from "src/composables/useLocalStorage";
+import { Notify } from "quasar";
+import { getLocalStorage, setLocalStorage } from "composables/useLocalStorage";
 
-export default {
-  name: "Agenda",
-  components: {
-    Calendar,
-  },
-  setup() {
-    const { mapCurrent } = useScreens({ xs: "0px", sm: "640px", md: "768px", lg: "1024px" });
-    const columns = mapCurrent({ lg: 3 }, 1);
-    const watchMovies = ref(getLocalStorage("watchMovies"));
-    const editMovieId = ref("");
-    const editMovieTitle = ref("");
-    const editCountryNameMovie = ref("");
-    const editDateMovie = ref("");
-    const events = ref([]);
-    const $q = ref(useQuasar());
-    const openAgendaDialog = ref(false);
+const { mapCurrent } = useScreens({ xs: "0px", sm: "640px", md: "768px", lg: "1024px" });
+const columns = mapCurrent({ lg: 3 }, 1);
+const watchMovies = ref(getLocalStorage("watchMovies"));
+const editMovieId = ref("");
+const editMovieTitle = ref("");
+const editCountryNameMovie = ref("");
+const editDateMovie = ref("");
+const events = ref([]);
+const openAgendaDialog = ref(false);
 
-    // NEW CODE
-    const listMode = ref(getLocalStorage("listMode") === "true");
-    const tableData = ref([]);
+// NEW CODE
+const listMode = ref(getLocalStorage("listMode") === "true");
+const tableData = ref([]);
 
-    // Definição das colunas da tabela
-    const tableColumns = [
-      { name: "title", label: "Title", align: "left", field: "title", sortable: true, sort: (a, b) => a.localeCompare(b) },
-      { name: "watchDate", label: "Watch Date", align: "center", field: "watchDate", sortable: true, sort: (a, b) => new Date(a) - new Date(b) },
-      { name: "watched", label: "Watched", align: "center", field: "watched" },
-      { name: "streaming", label: "Streaming", align: "center", field: "streamingList" },
-      { name: "actions", label: "Actions", align: "center", field: "actions" },
-    ];
+// Definição das colunas da tabela
+const tableColumns = [
+  { name: "title", label: "Title", align: "left", field: "title", sortable: true, sort: (a, b) => a.localeCompare(b) },
+  { name: "watchDate", label: "Watch Date", align: "center", field: "watchDate", sortable: true, sort: (a, b) => new Date(a) - new Date(b) },
+  { name: "watched", label: "Watched", align: "center", field: "watched" },
+  { name: "streaming", label: "Streaming", align: "center", field: "streamingList" },
+  { name: "actions", label: "Actions", align: "center", field: "actions" },
+];
 
-    const months = document.querySelectorAll(".vc-title");
+const months = document.querySelectorAll(".vc-title");
 
-    months.forEach((e) => {
-      // Create a new div element
-      const divElement = document.createElement("div");
-      divElement.className = "vc-title";
-      divElement.innerHTML = e.innerHTML;
+months.forEach((e) => {
+  // Create a new div element
+  const divElement = document.createElement("div");
+  divElement.className = "vc-title";
+  divElement.innerHTML = e.innerHTML;
 
-      // Replace the button with the new div
-      return e.parentNode.replaceChild(divElement, e);
-    });
+  // Replace the button with the new div
+  return e.parentNode.replaceChild(divElement, e);
+});
 
-    const clearCalendar = () => {
-      const hasMovies = watchMovies.value ? true : false;
-      let message = "There's no movies in Calendar.";
+const clearCalendar = () => {
+  const hasMovies = watchMovies.value ? true : false;
+  let message = "There's no movies in Calendar.";
 
-      hasMovies && ((watchMovies.value.length = 0), (events.value.length = 0), (tableData.value.length = 0), (message = "Calendar is clear!"));
+  hasMovies && ((watchMovies.value.length = 0), (events.value.length = 0), (tableData.value.length = 0), (message = "Calendar is clear!"));
 
-      return $q.value.notify({
-        type: "info",
-        message: message,
-      });
-    };
-
-    const addDates = () => {
-      if (watchMovies.value !== null) {
-        watchMovies.value.forEach((e) => {
-          const eventAdd = {
-            highlight: true,
-            isInteractive: true,
-            dates: [],
-            popover: {
-              label: "",
-              visibility: "click",
-            },
-            customData: {
-              movieId: "",
-              movieLink: "",
-              streamingCountry: String,
-              streamingList: Array,
-              watched: Boolean,
-            },
-          };
-
-          eventAdd.popover.label = e.movieTitle;
-          eventAdd.customData.movieId = e.movieID;
-          eventAdd.customData.movieLink = e.movieLink;
-          eventAdd.customData.streamingCountry = e.streamingCountryName;
-          eventAdd.customData.streamingList = e.streamingList;
-          eventAdd.customData.watched = e.watched;
-          eventAdd.dates.push(e.watchDate);
-
-          events.value.push(eventAdd);
-
-          // Defines table rows
-          tableData.value.push({
-            movieId: eventAdd.customData.movieId,
-            movieLink: eventAdd.customData.movieLink,
-            title: eventAdd.popover.label,
-            watchDate: new Date(e.watchDate).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" }),
-            watched: eventAdd.customData.watched,
-            streamingList: eventAdd.customData.streamingList,
-            streamingCountry: eventAdd.customData.streamingCountry,
-          });
-        });
-      }
-    };
-
-    addDates(); // Populates calendar on MOUNTED
-
-    const markWatch = (movieID, status) => {
-      let markMovie = events.value.filter((event) => event.customData.movieId === movieID)[0];
-
-      markMovie.customData.watched = status;
-      tableData.value.find((movie) => movie.movieId === movieID).watched = status;
-      watchMovies.value.find((movie) => movie.movieID === movieID).watched = status;
-    };
-
-    const movieWatchDateOpt = (movieWatchDateOpt) => {
-      return movieWatchDateOpt >= new Date().toISOString().split("T")[0].replace(/-/g, "/");
-    };
-
-    const openEditDialog = (movieID) => {
-      const movie = watchMovies.value.find((movie) => movie.movieID === movieID);
-
-      editMovieId.value = movieID;
-      editDateMovie.value = movie.watchDate;
-      editCountryNameMovie.value = movie.streamingCountryName;
-      editMovieTitle.value = movie.movieTitle;
-
-      return (openAgendaDialog.value = true);
-    };
-
-    const editMovieAgenda = () => {
-      let movieEdit = events.value.filter((event) => event.customData.movieId === editMovieId.value)[0];
-
-      movieEdit.dates = editDateMovie.value;
-      watchMovies.value.find((movie) => movie.movieID === editMovieId.value).watchDate = editDateMovie.value;
-      tableData.value.find((movie) => movie.movieId === editMovieId.value).watchDate = new Date(editDateMovie.value).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
-
-      openAgendaDialog.value = false;
-
-      return $q.value.notify({
-        type: "positive",
-        timeout: 2500,
-        message: "Movie edited successful.",
-      });
-    };
-
-    const delMovieAgenda = (movieID) => {
-      let moviesList = JSON.parse(localStorage.getItem("watchMovies"));
-
-      moviesList = moviesList.filter((movie) => movie.movieID !== movieID);
-
-      watchMovies.value = moviesList;
-      events.value = events.value.filter((event) => event.customData.movieId !== movieID);
-      tableData.value = tableData.value.filter((event) => event.movieId !== movieID);
-
-      return $q.value.notify({
-        type: "info",
-        timeout: 2500,
-        message: "Movie removed successful.",
-      });
-    };
-
-    const minDate = computed(() => {
-      return `${new Date().getFullYear()}-01-02`;
-    });
-
-    const maxDate = computed(() => {
-      return `${new Date().getFullYear() + 1}-02-01`;
-    });
-
-    watch(
-      [watchMovies.value, listMode],
-      ([newMovies, newListMode]) => {
-        // Updates watchMovies in localStorage
-        setLocalStorage("watchMovies", newMovies);
-
-        // Updates listMode in localStorage
-        setLocalStorage("listMode", newListMode);
-      },
-      { deep: true }
-    );
-
-    return {
-      columns,
-      editMovieTitle,
-      editCountryNameMovie,
-      editDateMovie,
-      events,
-      clearCalendar,
-      addDates,
-      markWatch,
-      movieWatchDateOpt,
-      openEditDialog,
-      editMovieAgenda,
-      delMovieAgenda,
-      openAgendaDialog,
-      minDate,
-      maxDate,
-      tableData,
-      tableColumns,
-      listMode,
-    };
-  },
+  return Notify.create({
+    type: "info",
+    message: message,
+  });
 };
+
+const addDates = () => {
+  if (watchMovies.value !== null) {
+    watchMovies.value.forEach((e) => {
+      const eventAdd = {
+        highlight: true,
+        isInteractive: true,
+        dates: [],
+        popover: {
+          label: "",
+          visibility: "click",
+        },
+        customData: {
+          movieId: "",
+          movieLink: "",
+          streamingCountry: String,
+          streamingList: Array,
+          watched: Boolean,
+        },
+      };
+
+      eventAdd.popover.label = e.movieTitle;
+      eventAdd.customData.movieId = e.movieID;
+      eventAdd.customData.movieLink = e.movieLink;
+      eventAdd.customData.streamingCountry = e.streamingCountryName;
+      eventAdd.customData.streamingList = e.streamingList;
+      eventAdd.customData.watched = e.watched;
+      eventAdd.dates.push(e.watchDate);
+
+      events.value.push(eventAdd);
+
+      // Defines table rows
+      tableData.value.push({
+        movieId: eventAdd.customData.movieId,
+        movieLink: eventAdd.customData.movieLink,
+        title: eventAdd.popover.label,
+        watchDate: new Date(e.watchDate).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" }),
+        watched: eventAdd.customData.watched,
+        streamingList: eventAdd.customData.streamingList,
+        streamingCountry: eventAdd.customData.streamingCountry,
+      });
+    });
+  }
+};
+
+addDates(); // Populates calendar on MOUNTED
+
+const markWatch = (movieID, status) => {
+  let markMovie = events.value.filter((event) => event.customData.movieId === movieID)[0];
+
+  markMovie.customData.watched = status;
+  tableData.value.find((movie) => movie.movieId === movieID).watched = status;
+  watchMovies.value.find((movie) => movie.movieID === movieID).watched = status;
+};
+
+const movieWatchDateOpt = (movieWatchDateOpt) => {
+  return movieWatchDateOpt >= new Date().toISOString().split("T")[0].replace(/-/g, "/");
+};
+
+const openEditDialog = (movieID) => {
+  const movie = watchMovies.value.find((movie) => movie.movieID === movieID);
+
+  editMovieId.value = movieID;
+  editDateMovie.value = movie.watchDate;
+  editCountryNameMovie.value = movie.streamingCountryName;
+  editMovieTitle.value = movie.movieTitle;
+
+  return (openAgendaDialog.value = true);
+};
+
+const editMovieAgenda = () => {
+  let movieEdit = events.value.filter((event) => event.customData.movieId === editMovieId.value)[0];
+
+  movieEdit.dates = editDateMovie.value;
+  watchMovies.value.find((movie) => movie.movieID === editMovieId.value).watchDate = editDateMovie.value;
+  tableData.value.find((movie) => movie.movieId === editMovieId.value).watchDate = new Date(editDateMovie.value).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
+
+  openAgendaDialog.value = false;
+
+  return Notify.create({
+    type: "positive",
+    timeout: 2500,
+    message: "Movie edited successful.",
+  });
+};
+
+const delMovieAgenda = (movieID) => {
+  let moviesList = JSON.parse(localStorage.getItem("watchMovies"));
+
+  moviesList = moviesList.filter((movie) => movie.movieID !== movieID);
+
+  watchMovies.value = moviesList;
+  events.value = events.value.filter((event) => event.customData.movieId !== movieID);
+  tableData.value = tableData.value.filter((event) => event.movieId !== movieID);
+
+  return Notify.create({
+    type: "info",
+    timeout: 2500,
+    message: "Movie removed successful.",
+  });
+};
+
+const minDate = computed(() => {
+  return `${new Date().getFullYear()}-01-02`;
+});
+
+const maxDate = computed(() => {
+  return `${new Date().getFullYear() + 1}-02-01`;
+});
+
+watch(
+  [watchMovies.value, listMode],
+  ([newMovies, newListMode]) => {
+    // Updates watchMovies in localStorage
+    setLocalStorage("watchMovies", newMovies);
+
+    // Updates listMode in localStorage
+    setLocalStorage("listMode", newListMode);
+  },
+  { deep: true }
+);
 </script>
 
 <style lang="scss" scoped>
