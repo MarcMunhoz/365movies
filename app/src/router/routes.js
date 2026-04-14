24ff9295d96be9a64b91b7c5f35965f601145a28
Marcelo Munhoz <me@marcelomunhoz.com>
2026-04-14 10:55:09 -0300

    refactor(router): Update page component route targets
    - Rename about/agenda pages to PascalCase component file names
    - Update router imports to AboutPage and AgendaPage

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -1,21 +1,33 @@
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue'), name: 'Home' },
-      { path: '/about', component: () => import('pages/About.vue'), name: 'About' },
-      { path: '/agenda', component: () => import('pages/Agenda.vue'), name: 'Agenda' }
-    ]
+      {
+        path: "",
+        component: () => import("pages/IndexPage.vue"),
+        name: "Home",
+      },
+      {
+        path: "/about",
+        component: () => import("pages/AboutPage.vue"),
+        name: "About",
+      },
+      {
+        path: "/agenda",
+        component: () => import("pages/AgendaPage.vue"),
+        name: "Agenda",
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
 export default routes
